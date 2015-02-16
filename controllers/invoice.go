b99b47c9101a650bcc6cbd13539f9ea48fbf40ab
Richard Gonzalez <damnpoet@gmail.com>
2015-02-16 03:37:31 -0500

    Adjusted invoice controller

--- controllers/invoice.go
+++ controllers/invoice.go
@@ -1,26 +1,41 @@
 package controllers
 
 import (
-	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
 
+	"encoding/json"
+	"io/ioutil"
+
+	"appengine"
+
+	"github.com/sam/roster/handler"
 	"github.com/sam/roster/models"
 	"github.com/sam/roster/validation"
-
-	"log"
 )
 
 // Operations about Users
 type InvoiceController struct {
 }
 
+func (c *InvoiceController) RegisterHandlers(r *mux.Router) {
+	r.Handle("/invoice/{uid}", handler.New(c.Get)).Methods("GET")
+	r.Handle("/invoice", handler.New(c.GetAll)).Methods("GET")
+	r.Handle("/invoice", handler.New(c.Post)).Methods("POST")
+	r.Handle("/invoice", handler.New(c.Put)).Methods("PUT")
+	r.Handle("/invoice/{uid}", handler.New(c.Delete)).Methods("DELETE")
+	r.Handle("/invoice/count", handler.New(c.Count)).Methods("GET")
+}
+
 // @Title Get
 // @Description get all Invoices
 // @Success 200 {object} models.Invoice
 // @router / [get]
-func (u *InvoiceController) GetAll() {
+func (controller *InvoiceController) GetAll(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
 	var invoices *[]models.Invoice
-	status := u.GetString("status")
-	page, sort, keyword := ParseParamsOfGetRequest(u.Input())
+
+	status := v["status"]
+	page, sort, keyword := ParseParamsOfGetRequest(v)
 
 	if keyword != "" {
 		invoices, _ = models.GetInvoiceByKeyword(status, keyword, page, sort, false, -1)
@@ -28,15 +43,8 @@ func (u *InvoiceController) GetAll() {
 	} else {
 		invoices, _ = models.GetAllInvoices(status, page, sort, false, -1)
 	}
-	u.Data["json"] = invoices
-	u.ServeJson()
-	/*
 
-
-		invoices := models.GetAllInvoices(status)
-		u.Data["json"] = invoices
-
-		u.ServeJson()*/
+	return invoices, nil
 }
 
 // @Title Get
@@ -45,17 +53,15 @@ func (u *InvoiceController) GetAll() {
 // @Success 200 {object} models.Invoice
 // @Failure 403 :uid is empty
 // @router /:uid [get]
-func (c *InvoiceController) Get() {
-	uid := c.GetString(":uid")
-	if uid != "" {
-		invoice, err := models.GetInvoice(uid)
-		if err != nil {
-			c.Data["json"] = err
-		} else {
-			c.Data["json"] = invoice
-		}
+func (controller *InvoiceController) Get(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
+	uid := v["uid"]
+
+	invoice, err := models.GetInvoice(uid)
+	if err != nil {
+		return nil, &handler.Error{err, "Error querying database", http.StatusInternalServerError}
 	}
-	c.ServeJson()
+
+	return invoice, nil
 }
 
 // @Title Get Count Invoices
@@ -63,18 +69,18 @@ func (c *InvoiceController) Get() {
 // @Param	keyword		string
 // @Success 200 {array} models.Invoice
 // @router /count [get]
-func (g *InvoiceController) GetCountAll() {
+func (controller *InvoiceController) Count(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
 	total := make(map[string]interface{})
-	status := g.GetString("status")
+	status := v["status"]
 	keyword := ""
-	if keywordP := g.GetString("keyword"); keywordP != "" {
+	if keywordP := v["keyword"]; keywordP != "" {
 		keyword = keywordP
 		_, total["total"] = models.GetInvoiceByKeyword(status, keyword, 1, "notSorting", true, -1)
 	} else {
 		_, total["total"] = models.GetAllInvoices(status, 1, "notSorting", true, -1)
 	}
-	g.Data["json"] = total
-	g.ServeJson()
+
+	return total, nil
 }
 
 // @Title Get Max Order Number from Invoices
@@ -82,11 +88,11 @@ func (g *InvoiceController) GetCountAll() {
 // @Param	status		string
 // @Success 200 int
 // @router /max-ordernumber [get]
-func (g *InvoiceController) GetMaxOrderNumber() {
+func (controller *InvoiceController) GetMaxOrderNumber(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
 	total := make(map[string]interface{})
 	total["max"] = models.GetMaxOrderNumber()
-	g.Data["json"] = total
-	g.ServeJson()
+
+	return total, nil
 }
 
 // @Title Get Resume from Invoices
@@ -94,12 +100,12 @@ func (g *InvoiceController) GetMaxOrderNumber() {
 // @Param	status		string
 // @Success int
 // @router /resume/:status [get]
-func (g *InvoiceController) GetInvoiceResume() {
-	status := g.GetString(":status")
+func (controller *InvoiceController) GetInvoiceResume(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
+	status := v["status"]
 	total := make(map[string]interface{})
 	total["amount"], total["cant"] = models.GetInvoiceResume(status)
-	g.Data["json"] = total
-	g.ServeJson()
+
+	return total, nil
 }
 
 // @Title updateInvoice
@@ -108,9 +114,15 @@ func (g *InvoiceController) GetInvoiceResume() {
 // @Success 200 {int} models.Invoice.Id
 // @Failure 403 body is empty
 // @router /:uid [put]
-func (g *InvoiceController) Put() {
+func (controller *InvoiceController) Put(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
+
+	data, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return nil, &handler.Error{err, "Could not read request", http.StatusBadRequest}
+	}
+
 	var invoice models.Invoice
-	json.Unmarshal(g.Ctx.Input.RequestBody, &invoice)
+	json.Unmarshal(data, &invoice)
 
 	user, _ := models.GetUser("5fbec591-acc8-49fe-a44e-46c59cae99f9") //TODO use user in session
 	invoice.Creator = user
@@ -119,19 +131,18 @@ func (g *InvoiceController) Put() {
 	valid := validation.Validation{}
 	b, err := valid.Valid(&invoice)
 	if err != nil {
-		log.Print(err)
-		g.CustomAbort(404, "Some errors on validation.")
+		return nil, &handler.Error{err, "Validation Errors", http.StatusBadRequest}
 	}
 	if !b {
 		for _, err := range valid.Errors {
-			g.CustomAbort(404, err.Message)
+			return nil, &handler.Error{nil, err.Message, http.StatusBadRequest}
 		}
-		g.CustomAbort(404, "Entity not found.")
+		return nil, &handler.Error{err, "Entity Not Found", http.StatusNoContent}
 	} else {
 		models.UpdateInvoice(&invoice)
 	}
-	g.Data["json"] = invoice
-	g.ServeJson()
+
+	return invoice, nil
 }
 
 // @Title delete
@@ -140,15 +151,17 @@ func (g *InvoiceController) Put() {
 // @Success 200 {string} delete success!
 // @Failure 403 uid is empty
 // @router /:uid [delete]
-func (c *InvoiceController) Delete() {
-	uid := c.GetString(":uid")
+func (c *InvoiceController) Delete(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
+	uid := v["uid"]
+
 	invoice, err := models.GetInvoice(uid)
 	if err != nil {
-		c.Abort("403")
+		return nil, &handler.Error{err, "Entity Not Found", http.StatusNoContent}
 	}
+
 	models.DeleteInvoice(invoice)
-	c.Data["json"] = "delete success!"
-	c.ServeJson()
+
+	return nil, nil
 }
 
 // @Title Create Invoice
@@ -157,9 +170,15 @@ func (c *InvoiceController) Delete() {
 // @Success 200 {int} models.Invoice.Id
 // @Failure 403 body is empty
 // @router / [post]
-func (g *InvoiceController) Post() {
+func (g *InvoiceController) Post(context appengine.Context, writer http.ResponseWriter, request *http.Request, v map[string]string) (interface{}, *handler.Error) {
+
+	data, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return nil, &handler.Error{err, "Could not read request", http.StatusBadRequest}
+	}
+
 	var invoice models.Invoice
-	json.Unmarshal(g.Ctx.Input.RequestBody, &invoice)
+	json.Unmarshal(data, &invoice)
 
 	user, _ := models.GetUser("5fbec591-acc8-49fe-a44e-46c59cae99f9") //TODO use user in session
 	invoice.Creator = user
@@ -169,17 +188,16 @@ func (g *InvoiceController) Post() {
 	valid := validation.Validation{}
 	b, err := valid.Valid(&invoice)
 	if err != nil {
-		log.Print(err)
-		g.CustomAbort(404, "Some errors on validation.")
+		return nil, &handler.Error{err, "Validation Errors", http.StatusBadRequest}
 	}
 	if !b {
 		for _, err := range valid.Errors {
-			g.CustomAbort(404, err.Message)
+			return nil, &handler.Error{nil, err.Message, http.StatusNoContent}
 		}
-		g.CustomAbort(404, "Entity not found.")
+		return nil, &handler.Error{err, "Entity Not Found", http.StatusNoContent}
 	} else {
 		models.AddInvoice(&invoice)
 	}
-	g.Data["json"] = invoice
-	g.ServeJson()
+
+	return invoice, nil
 }

b9f52a5725a930fb0844a3d9ee468eec75787715
Richard Gonzalez <damnpoet@gmail.com>
2015-02-23 22:48:26 -0500

    Added functionality for panel buttons

--- ui/app/src/modules/common/directives/PanelDirective.js
+++ ui/app/src/modules/common/directives/PanelDirective.js
@@ -11,13 +11,15 @@ angular.module('common').directive('panel', [function(){
         scope: {
             title: '@title',
             iconClass: '@iconClass',
-            type: '@type'
+            type: '@type',
+            refresh: '=refresh'
         },
         link: function($scope, element, attr) {
             $scope.panel = {
                 title: $scope.title,
                 iconClass: $scope.iconClass,
-                isPlain: false
+                isPlain: false,
+                visible: true
             };
 
             if(!_.isUndefined($scope.type) && panelTypes.indexOf($scope.type) > -1) {
@@ -29,6 +31,14 @@ angular.module('common').directive('panel', [function(){
 
             if(!_.isUndefined($scope.type) && $scope.type == "plain") {
                 $scope.panel.isPlain = true;
+            }
+
+            $scope.hasRefreshFunction = function() {
+                return _.isFunction($scope.refresh);
+            };
+
+            $scope.close = function() {
+                $scope.panel.visible = false;
             }
         }
     };

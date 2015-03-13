c3c0435efdab47996277cdbca2e345eeaacf66a5
Rolian Ruiz <rolian85@gmail.com>
2015-03-12 22:09:58 -0400

    - Please don't change this anymore.

--- ui/app/src/config.js
+++ ui/app/src/config.js
@@ -10,7 +10,7 @@ angular.module('config',[]).constant('config', {
         limitInPage: 20
     },
     api: {
-        baseUrl: 'http://192.168.1.100:9000'
+        baseUrl: 'http://localhost:8085'
     },
     date: {
         outputFormatDateTime: 'MMM D, YYYY[separator]HH:mm z',

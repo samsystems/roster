86a00b094351cf345b5eba1d334ed950573d6589
Rolian Ruiz <rolian85@gmail.com>
2015-03-25 00:15:06 -0400

    Common Services changes.

--- ui/app/src/modules/common/Module.js
+++ ui/app/src/modules/common/Module.js
@@ -8,61 +8,65 @@
  *
  * Common module of the application.
  */
+
 angular.module('common', [
     'ngRoute',
     'ngSanitize',
     'config'
 ])
-    .config(['$stateProvider', '$urlRouterProvider', '$injector', '$validationProvider', function ($stateProvider, $urlRouterProvider, $injector, $validationProvider) {
+.config(['$stateProvider', '$urlRouterProvider', '$translateProvider', '$validationProvider', '$injector', function ($stateProvider, $urlRouterProvider, $translateProvider, $validationProvider, $injector) {
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'src/modules/common/views/login.html',
+            controller: 'LoginController'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'src/modules/common/views/register.html',
+            controller: 'RegisterController'
+        })
+        .state('forgot', {
+            url: '/forgot',
+            templateUrl: 'src/modules/common/views/forgot.html',
+            controller: 'ForgotController'
+        })
+        .state('app', {
+            url: '/',
+            templateUrl: 'src/modules/common/views/app.html'
+        })
+        .state('404', {
+            url: '/404',
+            templateUrl: "src/modules/common/views/404.html"
+        });
 
         $urlRouterProvider.when('', '/login');
         $urlRouterProvider.when('/', '/dashboard');
         $urlRouterProvider.when('/admin', '/admin/users');
-
-        $stateProvider
-            .state('login', {
-                url: '/login',
-                templateUrl: 'src/modules/common/views/login.html',
-                controller: 'LoginController'
-            })
-            .state('register', {
-                url: '/register',
-                templateUrl: 'src/modules/common/views/register.html',
-                controller: 'RegisterController'
-            })
-            .state('forgot', {
-                url: '/forgot',
-                templateUrl: 'src/modules/common/views/forgot.html',
-                controller: 'ForgotController'
-            })
-            .state('app', {
-                url: '/',
-                templateUrl: 'src/modules/common/views/app.html'
-            })
-            .state('404', {
-                url: '/404',
-                templateUrl: "src/modules/common/views/404.html"
-            });
-
         $urlRouterProvider.otherwise('/404');
 
         $validationProvider.setExpression({
             uniqueRegisterEmail: function (value, scope, element, attrs) {
-                if (value) {
-                    var idValue = element.attr('data-value-id');
-                    console.log('asas');
-                    return $injector.get('CommonService').checkUniqueValue('User', 'email', value, idValue)
-                        .then(function (unique) {
-                            return unique.data.isUnique;
-                        });
-                }
-                else
-                    return true;
+               if (value) {
+                   var idValue = element.attr('data-value-id');
+                   console.log('asas');
+                   return $injector.get('CommonService').checkUniqueValue('User', 'email', value, idValue)
+                                       .then(function (unique) {
+                                           return unique.data.isUnique;
+                                       });
+               }
+               else
+                   return true;
             }
         }).setDefaultMsg({
             error: {
+                // TODO: make into a var
                 error: 'Email is already taken',
                 success: 'Email is available!'
             }
         });
-    }]);
+}]);
+//
+//    .config(['$stateProvider', '$urlRouterProvider', '$injector', '$validationProvider', function ($stateProvider, $urlRouterProvider, $injector, $validationProvider) {
+//
+//    }]);
--- ui/app/src/modules/invoice/Module.js
+++ ui/app/src/modules/invoice/Module.js
@@ -1,13 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name Dashboard
- * @description
- * The Dashboard module provides users with a dashboard that groups relevant graphs and information for the ship.
- *
- * Dashboard module.
- */
+
 angular.module('invoice', [
     'ngRoute',
     'ngSanitize',

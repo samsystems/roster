6f9df0b6a427cdb8c44210a8d01cc4709db41771
Joe Luis Borrero Senciales <jlborrero@gmail.com>
2015-03-29 12:53:12 -0500

    fix register

--- ui/app/src/modules/common/directives/NgUniqueDirective.js
+++ ui/app/src/modules/common/directives/NgUniqueDirective.js
@@ -6,31 +6,30 @@ angular.module("common").directive("ngUnique", function(CommonService,$validatio
         	    require: 'ngModel',
         	    link: function (scope, element, attrs, ngModel) {
                     element.bind('blur', function (e) {
-            	        if (!ngModel || !element.val()){ngModel.$setValidity('unique', false); return};
+            	        if (!ngModel || !element.val()){
+                            ngModel.$setValidity('uniqueValue', false);
+                            return
+                        };
             	        var keyProperty = scope.$eval(attrs.ngUnique);
             	        var currentValue = element.val();
                         var messageElem = element.closest('.form-group'),
-                            messageToShow = "Este elemento no es unico";
+                            messageToShow = "Email is already taken..";
                         messageElem.addClass('has-error');
 
                         if ($validation.showErrorMessage && messageToShow) {
                             scope.setMessage(ngModel.validationId, messageToShow);
                         }
-                        if(currentValue==='')
-                           ngModel.$setValidity('unique', false);
-                        else
-                            ngModel.$setValidity('unique', true);
-                        console.log('ok');
-                     /* CommonService.checkUniqueValue(keyProperty.key, keyProperty.property, currentValue)
+
+                      CommonService.checkUniqueValue(keyProperty.key, keyProperty.property, currentValue)
             	          .then(function (unique) {
                 	            //Ensure value that being checked hasn't changed
                 	            //since the Ajax call was made
                 	            if (currentValue == element.val()) {
                     	              console.log('unique = '+unique.data.isUnique);
-                    	              ngModel.$setValidity('unique', unique.data.isUnique);
+                    	              ngModel.$setValidity('uniqueValue', unique.data.isUnique);
                     	              scope.$broadcast('show-errors-check-validity');
                     	            }
-                	          });*/
+                	          });
             	      });
         	    }
     	  }

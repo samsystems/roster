8f0d972db401c3b64db0cf7fef48747e918000d1
Richard Gonzalez <damnpoet@gmail.com>
2015-05-02 13:59:03 -0400

    inverted condition

--- system/application.go
+++ system/application.go
@@ -41,9 +41,9 @@ func (application *Application) ConnectToDatabase() {
 
 	var dsn []string
 	if appengine.IsDevAppServer() {
-		dsn = []string{application.Configuration.Database.Host, "*", application.Configuration.Database.Name, application.Configuration.Database.User}
-	} else {
 		dsn = []string{application.Configuration.Database.User, password, "@", host, "/", application.Configuration.Database.Name}
+	} else {
+		dsn = []string{application.Configuration.Database.Host, "*", application.Configuration.Database.Name, application.Configuration.Database.User}
 	}
 
 	err = orm.RegisterDataBase("default", "mysql", strings.Join(dsn, ""), 30)

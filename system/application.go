b055c0ff646417a594d6592650051c6298b5d9b0
Rolian Ruiz <rolian85@gmail.com>
2015-05-04 22:48:52 -0400

    Test eppengine failing on isDevEnv.

--- system/application.go
+++ system/application.go
@@ -4,7 +4,7 @@ import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 
-	"appengine"
+	//	"appengine"
 	"github.com/golang/glog"
 	"strings"
 )
@@ -40,11 +40,11 @@ func (application *Application) ConnectToDatabase() {
 	}
 
 	var dsn []string
-	if appengine.IsDevAppServer() {
-		dsn = []string{application.Configuration.Database.User, password, "@", host, "/", application.Configuration.Database.Name}
-	} else {
-		dsn = []string{application.Configuration.Database.Host, "*", application.Configuration.Database.Name, application.Configuration.Database.User}
-	}
+	//	if appengine.IsDevAppServer() {
+	dsn = []string{application.Configuration.Database.User, password, "@", host, "/", application.Configuration.Database.Name}
+	//	} else {
+	//		dsn = []string{application.Configuration.Database.Host, "*", application.Configuration.Database.Name, application.Configuration.Database.User}
+	//	}
 
 	err = orm.RegisterDataBase("default", "mysql", strings.Join(dsn, ""), 30)
 
